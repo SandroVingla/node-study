eb2c81f17c9d10839c92c9679836b5d672928ed3
SandroVingla <sanvingla@hotmail.com>
2024-02-29 00:30:36 -0300

    study query params

--- src/server.js
+++ src/server.js
@@ -8,6 +8,12 @@ app.get("/message/:id/:user", (request, response) => {
   response.send(`Id da mensagen ${id} para o usuário ${user}`)
 })
 
+app.get("/users", (request,response) => {
+  const {page, limit} = request.query; 
+
+  response.send(`Pagina ${page}. Mostrar: ${limit}`)
+})
+
 const PORT = 3333;
 app.listen(PORT, () => console.log(`Server is runing on PORT ${PORT}`))
 
