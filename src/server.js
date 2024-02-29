3fdb677835ee46a369b2c50fc514ef5c3e516e39
SandroVingla <sanvingla@hotmail.com>
2024-02-29 00:15:16 -0300

    study raute params

--- src/server.js
+++ src/server.js
@@ -2,6 +2,12 @@ const express = require("express")
 
 const app = express();
 
+app.get("/message/:id/:user", (request, response) => {
+  const {id, user} = request.params;
+
+  response.send(`Id da mensagen ${id} para o usuário ${user}`)
+})
+
 const PORT = 3333;
 app.listen(PORT, () => console.log(`Server is runing on PORT ${PORT}`))
 
